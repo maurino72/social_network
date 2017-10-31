672f18d5fbdc2b1f5166446204e842ad0f8571bb
Bruno Maurino <brunomaurino27@gmail.com>
2017-10-31 12:08:40 -0300

    Creating and showing Publication's form

--- src/AppBundle/Controller/PublicationController.php
+++ src/AppBundle/Controller/PublicationController.php
@@ -2,16 +2,25 @@
 
 namespace AppBundle\Controller;
 
+use BackendBundle\Entity\Publication;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\PublicationType;
 
 class PublicationController extends Controller
 {
 
     public function indexAction(Request $request)
     {
-        return $this->render('AppBundle:Publication:home.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $publication = new Publication();
+
+        $form = $this->createForm(PublicationType::class, $publication);
+
+        return $this->render('AppBundle:Publication:home.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
 }
---
+++ src/AppBundle/Form/PublicationType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class PublicationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+
+            ->add('content', TextareaType::class, array(
+                'label' => 'Mensaje:',
+                'required' => 'required',
+                'attr' => array(
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('image', FileType::class, array(
+                'label' => 'Image:',
+                'required' => false,
+                'data_class' => null,
+                'attr' => array(
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('document', FileType::class, array(
+                'label' => 'Document:',
+                'required' => false,
+                'data_class' => null,
+                'attr' => array(
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('Create Publication', SubmitType::class, array(
+                'attr' => array(
+                    'class' => 'form-submit btn btn-info'
+                )
+            ))    ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BackendBundle\Entity\Publication'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'backendbundle_user';
+    }
+
+
+}
